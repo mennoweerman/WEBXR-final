ba668cb9bc2b4284d7c5e29a8b51009b25e14caa
menno weerman <menno.weerman@gmai.com>
2021-01-14 15:19:05 +0100

    veranderd 2e

--- ma-webxr/app.js
+++ ma-webxr/app.js
@@ -16,15 +16,36 @@ sphere.setRotation(0, -90.000, 0)
 sphere.setScale(0.175, 0.175, 0.175)
 
 // create some text
-const testText = new Text('Mediacollege Amsterdam WebXR');
-testText.setPosition(1.5, 0, -5);
-testText.setFontsize(10);
+const testText = new Text('Folkert de Jong');
+testText.setPosition(4.629, 1.792, -0.431);
+testText.setRotation(0.000, -90.000, 0.000);
+testText.setScale(0.175, 0.175, 0.175);
 
 // create an image
-const image = new XRImage('assets/images/FolkertdeJong.jpg');
+const image = new XRImage('assets/images/FolkertPortret.jpg');
 image.setPosition(1.955, 1.792, 1.109);
 image.setRotation(0.000, -90.000, 0.000);
 image.setScale(0.8, 0.8, 0.8);
+
+const image1 = new XRImage('assets/images/ballerina.JPG');
+image1.setPosition(-2.031, 1.794, 0.834);
+image1.setRotation(0.000, -90.000, 0.000);
+image1.setScale(0.8, 0.8, 0.8);
+
+const image2 = new XRImage('assets/images/balletjes.jpg');
+image2.setPosition(-0.082, 1.786, 2.285);
+image2.setRotation(0, 0, 0);
+image2.setScale(0.8, 0.8, 0.8);
+
+const image3 = new XRImage('assets/images/tafelEten.jpg');
+image3.setPosition(1.135, 1.828, -1.702);
+image3.setRotation(0, 0, 0);
+image3.setScale(0.8, 0.8, 0.8);
+
+const image4 = new XRImage('assets/images/JimFolkert.jpg');
+image4.setPosition(-1.230, 1.856, -1.697);
+image4.setRotation(0, 0, 0);
+image4.setScale(0.8, 0.8, 0.8);
 
 // create a 360 image
 const image360 = new Sky('assets/360_world.jpg');
@@ -46,7 +67,3 @@ AmbientLight.setPosition(-0.044, 0.910, 0.296);
 AmbientLight.setScale(0.1, 0.44443, 0.16213);
 AmbientLight.setRotation(0, 0, 0);
 
-const image1 = new XRImage('assets/images/ballerina.jpg');
-image1.setPosition(1.800, 1.400, 1.400);
-image1.setRotation(0.000, -90.000, 0.000);
-image1.setScale(0.8, 0.8, 0.8);
